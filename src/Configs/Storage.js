361191ffea8233ffc5b48cc07bc2f57f54ae4f2f
Mouhanad alaskari <mouhanad.alaskari@sheenvalue.com>
2021-04-24 18:18:10 +0300

    axios && endpoints && api configs

---
+++ src/Configs/Storage.js
@@ -0,0 +1,4 @@
+export const StorageKeys = {
+  token: "LABOSHOP_ADMIN_TOKEN",
+  userContext: "LABOSHOP_ADMIN_USER",
+};
---
+++ src/Services/Api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { create } from "apisauce";
+import ApiConfigs from "../Configs/Api";
+
+const axiosInstance = axios.create({
+  baseURL: ApiConfigs.apiUrl,
+  timeout: ApiConfigs.timeout,
+  headers: {
+    Accept: `application/json;v=${ApiConfigs.version}`,
+    "Content-Type": "application/json",
+  },
+});
+
+const Api = create({ axiosInstance: axiosInstance });
+const CancelToken = axios.CancelToken;
+const requestsSourceCancellation = CancelToken.source();
+
+const endpoints = {
+  general: {
+    configuration: (data, headers) =>
+      Api.get(`/configuration/${data.appID}/`,{}, {
+        ...headers,
+        cancelToken: requestsSourceCancellation.token,
+      }),
+  },
+  
+};
+
+export default endpoints;
